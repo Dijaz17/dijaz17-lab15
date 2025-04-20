73861b98fc75002a560adb88c5040b653d6defe4
Dijaz17 <87551242+Dijaz17@users.noreply.github.com>
2025-04-20 05:12:07 -0400

    completed all functionality

--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -16,6 +16,18 @@ public class EnigmaGUI {
             enigmaFrame.setOutputText(Comms.run(inputArgs));
         });
 
+        enigmaFrame.getDecryptButton().addActionListener(e -> {
+            String[] inputArgs = {
+                enigmaFrame.getInner(),
+                enigmaFrame.getMiddle(),
+                enigmaFrame.getOuter(),
+                enigmaFrame.getInitialPosition(),
+                "decrypt",
+                enigmaFrame.getInputText()
+            };
+
+            enigmaFrame.setOutputText(Comms.run(inputArgs));
+        });
 
     }
 }
